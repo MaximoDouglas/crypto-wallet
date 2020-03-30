c975045a9620b73a675ed3d32cfb7ac882163352
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-03-29 23:01:29 -0300

    origin/master Refactor db:seed to create not repeated coins
    - Add fin_or_create_by call to not repeate coins
    - Add a list of coins to be iterated
    - Add spinner on seeds task
    
    Signed-off-by: MaximoDouglas <douglas.h.maximo@gmail.com>

--- crypto_wallet/db/seeds.rb
+++ crypto_wallet/db/seeds.rb
@@ -5,21 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+spinner = TTY::Spinner.new("[:spinner] Creating seed coins...")
+spinner.auto_spin
 
-Coin.create!(
-    description: "Bitcoin",
-    acronym: "BTC",
-    url_image: "https://pngimg.com/uploads/bitcoin/bitcoin_PNG47.png"
-)
+coins = [{  
+            description: "Bitcoin", 
+            acronym: "BTC",
+            url_image: "https://pngimg.com/uploads/bitcoin/bitcoin_PNG47.png"
+            },
+            {
+            description: "Ethereum",
+            acronym: "ETH",
+            url_image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Ethereum_logo_2014.svg/628px-Ethereum_logo_2014.svg.png"
+            }, 
+            {   
+            description: "Dash",
+            acronym: "DASH",
+            url_image: "https://pngimage.net/wp-content/uploads/2018/05/dash-coin-logo-png-6.png"
+        }]
 
-Coin.create!(
-    description: "Ethereum",
-    acronym: "ETH",
-    url_image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Ethereum_logo_2014.svg/628px-Ethereum_logo_2014.svg.png"
-)
+coins.each do |coin|
+    Coin.find_or_create_by!(
+        description: coin[:description], 
+        acronym: coin[:acronym],
+        url_image: coin[:url_image]
+    )
+end
 
-Coin.create!(
-    description: "Dash",
-    acronym: "DASH",
-    url_image: "https://pngimage.net/wp-content/uploads/2018/05/dash-coin-logo-png-6.png"
-)
+spinner.success('Done!')
