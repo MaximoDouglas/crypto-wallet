096fe71550c5085855fbe412938ca17619ea885a
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-04-15 20:08:06 -0300

    Add optional modifier on Contact model
    - Add optional modifier to the relation of Contact and Kind on Contact model
    
    Signed-off-by: MaximoDouglas <douglas.h.maximo@gmail.com>

--- notebook-api/app/models/contact.rb
+++ notebook-api/app/models/contact.rb
@@ -1,12 +1,12 @@
 class Contact < ApplicationRecord
-    belongs_to :kind
+    belongs_to :kind, optional: true
 
     def author
         "Doug"
     end
 
     def kind_description
-        self.kind.description
+        self.kind != nil ? self.kind.description : ""
     end
 
     def as_json(options={})
