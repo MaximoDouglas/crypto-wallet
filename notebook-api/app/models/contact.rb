1991631904c4e4b024e698dc65f9803519e8095d
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-04-21 20:35:28 -0300

    Add relationships with Active Model Serializers
    -
    
    Signed-off-by: MaximoDouglas <douglas.h.maximo@gmail.com>

--- notebook-api/app/models/contact.rb
+++ notebook-api/app/models/contact.rb
@@ -1,10 +1,9 @@
 class Contact < ApplicationRecord
     belongs_to :kind, optional: true
+    has_many   :phones
+    has_one    :address
     
-    has_many :phones
     accepts_nested_attributes_for :phones, allow_destroy: true
-    
-    has_one :address
     accepts_nested_attributes_for :address, update_only: true
 
     def author
