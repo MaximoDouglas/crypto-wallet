82476bcf75c8ace8a311acf9138e9f03cf9ac0a6
MaximoDouglas <douglas.h.maximo@gmail.com>
2020-04-21 22:37:06 -0300

    Add HATEOAS links to kinds

--- notebook-api/app/serializers/contact_serializer.rb
+++ notebook-api/app/serializers/contact_serializer.rb
@@ -1,9 +1,12 @@
 class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate
+  
+  has_many :phones
+  has_one  :address
 
-  belongs_to :kind
-  has_many   :phones
-  has_one    :address
+  belongs_to :kind do 
+    link(:kind) { kind_url(object.kind.id) }
+  end
 
   meta do 
     { 
